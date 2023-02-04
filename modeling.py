6f77103338b7479d1213328e2bebcb6d5572f3a2
Serkankizilirmak <serkankizilirmaak@gmail.com>
2023-02-04 23:24:16 +0300

    4.02.23

---
+++ modeling.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold, RepeatedStratifiedKFold, train_test_split, cross_val_score
+from sklearn.preprocessing import (OrdinalEncoder, OneHotEncoder, LabelEncoder,
+                                   StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler,
+                                   PowerTransformer) # convert to Gaussian-like data
